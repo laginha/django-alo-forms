4eb00b6595bb4ed1b9386e1dc53aa904ca1e31e0
Diogo Machado <diogo.laginha.machado@gmail.com>
2015-03-19 11:52:30 +0000

    add new meta options: multifield_lookups and no_defaults

--- src/alo/mixins.py
+++ src/alo/mixins.py
@@ -6,10 +6,10 @@ from .operators import BaseOperator, AND, OR
 class QueryFormMixin(object):
 
     @property
-    def non_empty_data(self):
-        if not hasattr(self, '_non_empty_data'):
+    def validated_data(self):
+        if not hasattr(self, '_validated_data'):
             self.set_parameters()
-        return self._non_empty_data
+        return self._validated_data
 
     @property
     def parameters(self):
@@ -17,36 +17,56 @@ class QueryFormMixin(object):
             self.set_parameters()
         return self._parameters
     
+    def get_data(self, name):
+        value = self.cleaned_data.get(name, None)
+        return value if value else self._meta.defaults.get(name, None)
+    
     def set_parameters(self):
         self._parameters = {}
-        self._non_empty_data = {}
-        items = self.fields.iteritems()
-        name_to_lookups = {
-            name: self._meta.lookups[name] for name,field in items
-        }
-        for name,value in self.cleaned_data.iteritems():
+        self._validated_data = {}
+        for fieldname,lookup in self._meta.lookups.iteritems():
+            value = self.get_data(fieldname)
             if value:
-                self._non_empty_data[name] = value
-                for each in name_to_lookups[name]:
-                    self._parameters[ each ] = value
+                self._validated_data[fieldname] = value
+                self._parameters[self._meta.lookups[fieldname]] = value
+        for fields,call in self._meta.multifield_lookups.iteritems():
+            values = []
+            for fieldname in fields:
+                value = self.get_data(fieldname)
+                if value:
+                    self._validated_data[fieldname] = value
+                    self._parameters.pop(self._meta.lookups[fieldname])
+                    values.append(value)
+            if len(values) == len(fields):
+                self._parameters.update(call(*values))
     
-    def set_meta(self):
+    def set_meta(self):                        
         if not hasattr(self, '_meta'):
             self._meta = type('Meta', (object,), {})()
-        self.set_lookups()
+        if not hasattr(self, 'Meta'):
+            self.Meta = type('Meta', (object,), {})()
+        self.set_lookups_and_defaults()
+        self.set_multifield_lookups()
         self.set_extralogic()
     
-    def set_lookups(self):
+    def set_lookups_and_defaults(self):
+        self._meta.defaults = {}
         self._meta.lookups = {}
+        self._meta.no_defaults = getattr(self.Meta, 'no_defaults', False)
         for name,field in self.fields.iteritems():
-             self._meta.lookups[name] = (name,)
+             self._meta.lookups[name] = name
+             if not self._meta.no_defaults:
+                 self._meta.defaults[name] = field.initial
         if hasattr(self.Meta, 'lookups'):
-            for fieldname,alias in self.Meta.lookups.items():
-                if isinstance(alias, (str, unicode)):
-                    self._meta.lookups[fieldname] = (alias,)
-                else:
-                    self._meta.lookups[fieldname] = alias
-                
+            for fieldname,alias in self.Meta.lookups.iteritems():
+                self._meta.lookups[fieldname] = alias
+    
+    def set_multifield_lookups(self):
+        self._meta.multifield_lookups = {}
+        if hasattr(self.Meta, 'multifield_lookups'):
+            for fields,alias in self.Meta.multifield_lookups.iteritems():
+                self._meta.multifield_lookups[fields] = alias
+    
     def set_extralogic(self):
         def get_logic(operator):
             args = []
@@ -57,7 +77,7 @@ class QueryFormMixin(object):
                 elif isinstance(value, BaseOperator):
                     args.append( get_logic(value) )
             return operator.__class__.create(*args)
-        
+            
         self._meta.extralogic = []
         if hasattr(self.Meta, 'extralogic'):
             for each in self.Meta.extralogic:
@@ -66,6 +86,6 @@ class QueryFormMixin(object):
     def clean_extralogic(self):
         for each in self._meta.extralogic:
             try:
-                each.is_valid(self.cleaned_data)
+                each.is_valid(self.validated_data)
             except ValidationError as e:
                 self.add_error(e.subject, e.message)
